dbbca3bab18af42c2533e1257bd3c73933bb7f83
Paul <128304338+UCC-Paul@users.noreply.github.com>
2024-05-05 02:22:14 +0800

    DELETE INBOX FILE NOT USED FIXED APPLICATION IMG FETCH

--- balaifinder-app/src/realtor/Application.jsx
+++ balaifinder-app/src/realtor/Application.jsx
@@ -117,6 +117,20 @@ const ApplicationList = () => {
                                         strokeLinejoin="round" className="flex-shrink-0 w-6 h-6 text-emerald-500" aria-hidden="true">
                                         <polyline points="20 6 9 17 4 12"></polyline>
                                     </svg><span className="ml-3 ">{application.email}</span></li>
+                                <li className="flex"><svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"
+                                        fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round"
+                                        strokeLinejoin="round" className="flex-shrink-0 w-6 h-6 text-emerald-500" aria-hidden="true">
+                                        <polyline points="20 6 9 17 4 12"></polyline>
+                                    </svg>
+                                    <span className="ml-3 ">Certificate:</span>
+                                    <img className="h-20 w-20" src={application.certificate} alt="" /></li>
+                                <li className="flex"><svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"
+                                        fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round"
+                                        strokeLinejoin="round" className="flex-shrink-0 w-6 h-6 text-emerald-500" aria-hidden="true">
+                                        <polyline points="20 6 9 17 4 12"></polyline>
+                                    </svg>
+                                    <span className="ml-3 ">Company ID:</span>
+                                    <img className="h-20 w-20" src={application.companyid} alt="" /></li>
                             </ul>
                         </div>
                         <button onClick={() => handleUpdateButtonClick(application)} className="bg-sky-500 text-white hover:bg-sky-700 mt-8 block w-full py-3 px-6 border border-transparent rounded-md text-center font-medium">UPDATE APPLICATION STATUS</button>
