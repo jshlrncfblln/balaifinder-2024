310fc4d50210d5037eafb718318bd94d30b8f5ec
LYKAMAEEE <joshua.laurence.fabi@gmail.com>
2024-05-10 01:57:23 +0800

    update

--- balaifinder-app/src/components/PropertyList.jsx
+++ balaifinder-app/src/components/PropertyList.jsx
@@ -92,30 +92,32 @@ export default function PropLists({ page, limit, priceFilter, locationFilter, pr
                             const { text, bgClass } = getBadgeInfo();
 
                             return (
-                                <div key={item.id} className="w-72 bg-white shadow-md shadow-black rounded-lg duration-500 hover:scale-105">
-                                    {/* Badge */}
+                                <div>
                                     <div className="absolute top-0 right-0 mt-2 mr-2">
                                         <span className={`inline-block rounded-full px-3 py-1 text-sm font-semibold text-gray-700 ${bgClass}`}>
                                             {text}
                                         </span>
                                     </div>
-                                    <Link to={`/details/${item.id}`}>
-                                        <img src={item.imgsrc ? item.imgsrc : 'https://img.freepik.com/free-photo/house-isolated-field_1303-23773.jpg?t=st=1710318322~exp=1710321922~hmac=1797b6b00add732c13f15b3160cb99f3c7e6fe2e9fb745a53d801c74a968fe8b&w=1380'} alt="Property" className="h-52 w-full" />
-                                        <div className="px-4 py-3 w-72">
-                                            <span className="text-gray-400 mr-3 uppercase text-xs">{item.type}</span>
-                                            <p className="text-lg font-bold text-black truncate block capitalize">{item.name}</p>
-                                            <p className="text-lg font-bold text-black truncate block capitalize">{item.location}</p>
-                                            <div className="flex items-center">
-                                                <p className="text-lg font-semibold text-black cursor-auto my-3">₱{new Intl.NumberFormat().format(item.price)}</p>
-                                                <div className="ml-auto">
-                                                    <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="currentColor" className="bi bi-bag-plus" viewBox="0 0 16 16">
-                                                        <path fillRule="evenodd" d="M8 7.5a.5.5 0 0 1 .5.5v1.5H10a.5.5 0 0 1 0 1H8.5V12a.5.5 0 0 1-1 0v-1.5H6a.5.5 0 0 1 0-1h1.5V8a.5.5 0 0 1 .5-.5z"/>
-                                                        <path d="M8 1a2.5 2.5 0 0 1 2.5 2.5V4h-5v-.5A2.5 2.5 0 0 1 8 1zm3.5 3v-.5a3.5 3.5 0 1 0-7 0V4H1v10a2 2 0 0 0 2 2h10a2 2 0 0 0 2-2V4h-3.5zM2 5h12v9a1 1 0 0 1-1 1H3a1 1 0 0 1-1-1V5z"/>
-                                                    </svg>
+                                    <div key={item.id} className="w-72 bg-white shadow-md shadow-black rounded-lg duration-500 hover:scale-105">
+                                        {/* Badge */}
+                                        <Link to={`/details/${item.id}`}>
+                                            <img src={item.imgsrc ? item.imgsrc : 'https://img.freepik.com/free-photo/house-isolated-field_1303-23773.jpg?t=st=1710318322~exp=1710321922~hmac=1797b6b00add732c13f15b3160cb99f3c7e6fe2e9fb745a53d801c74a968fe8b&w=1380'} alt="Property" className="h-52 w-full" />
+                                            <div className="px-4 py-3 w-72">
+                                                <span className="text-gray-400 mr-3 uppercase text-xs">{item.type}</span>
+                                                <p className="text-lg font-bold text-black truncate block capitalize">{item.name}</p>
+                                                <p className="text-lg font-bold text-black truncate block capitalize">{item.location}</p>
+                                                <div className="flex items-center">
+                                                    <p className="text-lg font-semibold text-black cursor-auto my-3">₱{new Intl.NumberFormat().format(item.price)}</p>
+                                                    <div className="ml-auto">
+                                                        <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="currentColor" className="bi bi-bag-plus" viewBox="0 0 16 16">
+                                                            <path fillRule="evenodd" d="M8 7.5a.5.5 0 0 1 .5.5v1.5H10a.5.5 0 0 1 0 1H8.5V12a.5.5 0 0 1-1 0v-1.5H6a.5.5 0 0 1 0-1h1.5V8a.5.5 0 0 1 .5-.5z"/>
+                                                            <path d="M8 1a2.5 2.5 0 0 1 2.5 2.5V4h-5v-.5A2.5 2.5 0 0 1 8 1zm3.5 3v-.5a3.5 3.5 0 1 0-7 0V4H1v10a2 2 0 0 0 2 2h10a2 2 0 0 0 2-2V4h-3.5zM2 5h12v9a1 1 0 0 1-1 1H3a1 1 0 0 1-1-1V5z"/>
+                                                        </svg>
+                                                    </div>
                                                 </div>
                                             </div>
-                                        </div>
-                                    </Link>
+                                        </Link>
+                                    </div>
                                 </div>
                             );
                         })
