480795291531cf5a87f51e2418626e9522cb0ab3
Paul <128304338+UCC-Paul@users.noreply.github.com>
2024-05-11 01:17:21 +0800

    NEW SERVER CONNECTOR
    USED VERCEL

--- balaifinder-app/backend-connector.js
+++ balaifinder-app/backend-connector.js
@@ -1,8 +1,8 @@
 //OLD SERVER IN RENDER
 
-export const backendurl = "https://balaifinder-backend-deploy.onrender.com"
+//export const backendurl = "https://balaifinder-backend-deploy.onrender.com"
 
 //NEW SERVER VERCEL
-//export const backendurl = "https://balaifinder-liveserver.vercel.app"
+export const backendurl = "https://balaifinder-server.vercel.app"
 
 //paulpogi
