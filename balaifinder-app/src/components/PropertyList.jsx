e070b855609c35c4b2bbfef36507d6266aecbb9c
Hev Fabi <joshua.laurence.fabi@gmail.com>
2024-05-10 18:22:43 +0800

    Update PropertyList.jsx

--- balaifinder-app/src/components/PropertyList.jsx
+++ balaifinder-app/src/components/PropertyList.jsx
@@ -57,17 +57,17 @@ export default function PropLists({ page, limit, priceFilter, locationFilter, pr
   }; */
 
     const SkeletonCard = () => {
-        <div
-        class="flex flex-col bg-neutral-300 w-56 h-64 animate-pulse rounded-xl p-4 gap-4"
-      >
-        <div class="bg-neutral-400/50 w-full h-32 animate-pulse rounded-md"></div>
-        <div class="flex flex-col gap-2">
-          <div class="bg-neutral-400/50 w-full h-4 animate-pulse rounded-md"></div>
-          <div class="bg-neutral-400/50 w-4/5 h-4 animate-pulse rounded-md"></div>
-          <div class="bg-neutral-400/50 w-full h-4 animate-pulse rounded-md"></div>
-          <div class="bg-neutral-400/50 w-2/4 h-4 animate-pulse rounded-md"></div>
-        </div>
-      </div>  
+        return(
+            <div class="flex flex-col bg-neutral-300 w-56 h-64 animate-pulse rounded-xl p-4 gap-4">
+                <div class="bg-neutral-400/50 w-full h-32 animate-pulse rounded-md"></div>
+                <div class="flex flex-col gap-2">
+                <div class="bg-neutral-400/50 w-full h-4 animate-pulse rounded-md"></div>
+                <div class="bg-neutral-400/50 w-4/5 h-4 animate-pulse rounded-md"></div>
+                <div class="bg-neutral-400/50 w-full h-4 animate-pulse rounded-md"></div>
+                <div class="bg-neutral-400/50 w-2/4 h-4 animate-pulse rounded-md"></div>
+                </div>
+            </div>
+        )  
     }
     return (
         <div className="w-fit mx-auto mt-10 mb-10">
